7be90991bb365eb8849cf4864ec166a0aa8d3f8e
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2023-05-23 16:38:44 -0300

    FEAT: styled-components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,15 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+align-items: center;
+justify-content: center;
+flex: 1;
+display: flex;
+background-color: #000;
+`
+
+export const Title = styled.Text`
+color: #fff;
+font-size: 100px;
+font-weight: bold;
+`
