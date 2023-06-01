c21565d408b33adbfc22059037bcce98caa888e5
Davi Henrique <63513686+davihr1@users.noreply.github.com>
2023-06-01 18:44:55 -0300

    Component ListEmpty

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,19 +2,32 @@ import { Header } from '@components/Header';
 import * as S from './styles';
 import { HighLight } from '@components/Highlight';
 import { GroupsCard } from '@components/GroupCard';
+import { useState } from 'react';
+import { Alert, FlatList } from 'react-native';
+import { ListEmpty } from '@components/ListEmpity';
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([])
   return (
     <S.Container>
       <Header  />
+
 
       <HighLight 
       title="Turmas"
       subtitle="Jogue com sua turma"
        />
-
-       <GroupsCard title="Galera do ignite" />
-       <GroupsCard title="Galera do ignite" />
+       <FlatList 
+       data={groups}
+       keyExtractor={item => item}
+       contentContainerStyle={groups.length === 0 && { flex: 1 }}
+       renderItem={({ item }) => (
+        <GroupsCard title={item}
+        onPress={() => Alert.alert(item)}
+        />
+       )}
+       ListEmptyComponent={() => <ListEmpty message="Cadastre sua primeira turma" />}
+       />
     </S.Container>
   );
 }
